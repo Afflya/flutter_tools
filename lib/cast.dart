c0ccf0e0785c2e853144d8a9d63e51e7b732d4b9
afflya <afflyas.view@gmail.com>
2025-04-24 16:20:17 +0300

    feat: update cast ext. Added cast ext for List. Added ext for JsonObject

--- lib/cast.dart
+++ lib/cast.dart
@@ -1,7 +1,35 @@
-extension CastExt on dynamic {
-  T? asOrNull<T>() => this is T ? this : null;
+extension CastExt on Object? {
+  T cast<T>() => this as T;
 
-  T as<T>() => this as T;
+  T? castOrNull<T>() => this is T ? cast<T>() : null;
 }
 
-T? tryCast<T>(x) => x is T ? x : null;
+extension ListCastExt on List<dynamic> {
+
+  List<T> castList<T>() {
+    return List<T>.from(this);
+  }
+
+  List<T>? castListOrNull<T>() {
+    try {
+      // For non-nullable T, reject any null values
+      if (null is! T && contains(null)) {
+        return null;
+      }
+
+      // Check if all elements are of type T
+      for (final element in this) {
+        if (element is! T && element != null) {
+          return null;
+        }
+      }
+      // If all checks passed, cast the list
+      return List<T>.from(this);
+    } catch (e) {
+      return null;
+    }
+  }
+
+}
+
+T? castOrNull<T>(x) => x is T ? x : null;
--- lib/json.dart
+++ lib/json.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter_tools/cast.dart';
+
 mixin JsonX {}
 
 typedef JsonObject = Map<String, dynamic>;
@@ -9,4 +11,28 @@ extension JsonStringExt on String {
   JsonObject get toJsonObject => json.decode(this) as JsonObject;
 
   JsonArray get toJsonArray => json.decode(this) as JsonArray;
+}
+
+extension JsonObjectExt on JsonObject {
+  Object? getObj(String key) => (this[key] as Object?);
+
+  T? getAsType<T>(String key) => getObj(key)?.castOrNull();
+
+  JsonObject? getJsonObj(String key) => this[key] as JsonObject?;
+
+  List<T>? getListOfType<T>(String key) {
+    return (this[key] as JsonArray?)?.castList();
+  }
+
+  List<T>? getListOfTypeOrNull<T>(String key) {
+    return (this[key] as JsonArray?)?.castListOrNull();
+  }
+
+  List<JsonObject>? getListOfJson(String key) {
+    return (this[key] as JsonArray?)?.castList();
+  }
+
+  List<JsonObject>? getListOfJsonOrNull(String key) {
+    return (this[key] as JsonArray?)?.castListOrNull();
+  }
 }
